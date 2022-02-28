5eeb98222f61db751074cfdf5423a7d2d582212c
libin v Baby <82673070+libinvbaby@users.noreply.github.com>
2022-02-28 17:41:43 +0530

    first commit

---
+++ contact.js
@@ -0,0 +1,90 @@
+function validateName() {
+
+    var name = document.getElementById('contact-name').value;
+
+    if(name.length == 0) {
+
+      producePrompt('Name is required', 'name-error' )
+      return false;
+
+  }
+
+
+  return true;
+
+}
+
+function validatePhone() {
+
+var phone = document.getElementById('contact-phone').value;
+
+if(phone.length == 0) {
+    producePrompt('Phone number is required.', 'phone-error', 'red');
+    return false;
+}
+
+if(phone.length != 10) {
+    producePrompt('Include area code.', 'phone-error', 'red');
+    return false;
+}
+
+if(!phone.match(/^[0-9]{10}$/)) {
+    producePrompt('Only digits, please.' ,'phone-error', 'red');
+    return false;
+}
+
+return true;
+
+}
+
+function validateEmail () {
+
+var email = document.getElementById('contact-email').value;
+
+if(email.length == 0) {
+
+  producePrompt('Email Invalid','email-error', 'red');
+  return false;
+
+}
+
+if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+
+  producePrompt('Email Invalid', 'email-error', 'red');
+  return false;
+
+}
+
+return true;
+
+}
+
+
+
+function validateForm() {
+if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+  jsShow('submit-error');
+  producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+  setTimeout(function(){jsHide('submit-error');}, 2000);
+  return false;
+}
+else {
+
+}
+}
+
+function jsShow(id) {
+document.getElementById(id).style.display = 'block';
+}
+
+function jsHide(id) {
+document.getElementById(id).style.display = 'none';
+}
+
+
+function producePrompt(message, promptLocation, color) {
+
+document.getElementById(promptLocation).innerHTML = message;
+document.getElementById(promptLocation).style.color = color;
+
+}
